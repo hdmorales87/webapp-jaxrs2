2595da1b35f962e6b9a487b1646b38b8a0a20c3d
hector morales <hector.morales@logicalsoft.co>
2022-01-26 19:17:44 -0500

    primer commit

---
+++ src/main/java/org/hdmorales/webapp/jaxws/repositories/CursoRepository.java
@@ -0,0 +1,12 @@
+package org.hdmorales.webapp.jaxws.repositories;
+
+import org.hdmorales.webapp.jaxws.models.Curso;
+
+import java.util.List;
+
+public interface CursoRepository {
+    List<Curso> listar();
+    Curso guardar(Curso curso);
+    Curso porId(Long id);
+    void eliminar(Long id);
+}
